972e2eea08b7c4479d845d7e31f2d57154a650cf
alex-hjm <2840496970@qq.com>
2022-06-05 00:09:38 +0800

    update common

--- base/common.h
+++ base/common.h
@@ -3,20 +3,14 @@
  * @author hjm (hjmalex@163.com)
  * @version 1.0
  * @date 2022-05-08
- *
- * @copyright Copyright (c) 2022 hjm
- *
  */
 #ifndef CT_BASE_COMMON_H
 #define CT_BASE_COMMON_H
 
 #include "base/exports.h"
 
-namespace ct {
-/**
- * @brief 将颜色从HSV格式转换为RGB格式。
- */
-void CT_EXPORT HSVtoRGB(float h, float s, float v, float &r, float &g,
-                        float &b);
-}  // namespace ct
-#endif  // CT_BASE_COMMON_H
+namespace ct
+{
+    void CT_EXPORT HSVtoRGB(float h, float s, float v, float &r, float &g, float &b);
+} // namespace ct
+#endif // CT_BASE_COMMON_H
