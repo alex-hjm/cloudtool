a7b4f84377f8e09a08e3478c8fcb3b87f06ba65f
alex-hjm <2840496970@qq.com>
2022-06-08 02:58:13 +0800

    modify rgb

--- base/cloud.h
+++ base/cloud.h
@@ -16,14 +16,14 @@
 
 #include "base/exports.h"
 
-#define CLOUD_DEFAULT_ID    "cloud"
-#define BOX_DEFAULT_ID      "-box"
-#define NORMALS_DEFAULT_ID  "-normals"
+#define CLOUD_DEFAULT_ID "cloud"
+#define BOX_DEFAULT_ID "-box"
+#define NORMALS_DEFAULT_ID "-normals"
 
-#define CLOUD_TYPE_XYZ      "XYZ"
-#define CLOUD_TYPE_XYZRGB   "XYZRGB"
-#define CLOUD_TYPE_XYZN     "XYZNormal"
-#define CLOUD_TYPE_XYZRGBN  "XYZRGBNormal"
+#define CLOUD_TYPE_XYZ "XYZ"
+#define CLOUD_TYPE_XYZRGB "XYZRGB"
+#define CLOUD_TYPE_XYZN "XYZNormal"
+#define CLOUD_TYPE_XYZRGBN "XYZRGBNormal"
 
 namespace ct
 {
@@ -34,35 +34,35 @@ namespace ct
 
     struct Box
     {
-        double              width;
-        double              height;
-        double              depth;
-        Eigen::Affine3f     pose;
-        Eigen::Vector3f     translation;
-        Eigen::Quaternionf  rotation;
+        double width;
+        double height;
+        double depth;
+        Eigen::Affine3f pose;
+        Eigen::Vector3f translation;
+        Eigen::Quaternionf rotation;
     };
 
     class CT_EXPORT Cloud : public pcl::PointCloud<PointXYZRGBN>
     {
     public:
         Cloud() : m_id(CLOUD_DEFAULT_ID),
-            m_box_rgb(255, 255, 255),
-            m_normals_rgb(255, 255, 255),
-            m_type(CLOUD_TYPE_XYZ),
-            m_point_size(1),
-            m_opacity(1.0),
-            m_resolution(0.0) {}
+                  m_box_rgb(255, 255, 255),
+                  m_normals_rgb(255, 255, 255),
+                  m_type(CLOUD_TYPE_XYZ),
+                  m_point_size(1),
+                  m_opacity(1.0),
+                  m_resolution(0.0) {}
 
-        Cloud(const Cloud& cloud, const Indices& indices)
+        Cloud(const Cloud &cloud, const Indices &indices)
             : pcl::PointCloud<PointXYZRGBN>(cloud, indices) {}
 
-        Cloud& operator+=(const Cloud& rhs)
+        Cloud &operator+=(const Cloud &rhs)
         {
             concatenate((*this), rhs);
             return (*this);
         }
 
-        Cloud operator+(const Cloud& rhs) { return (Cloud(*this) += rhs); }
+        Cloud operator+(const Cloud &rhs) { return (Cloud(*this) += rhs); }
 
         using Ptr = std::shared_ptr<Cloud>;
         using ConstPtr = std::shared_ptr<const Cloud>;
@@ -152,17 +152,17 @@ namespace ct
         /**
          * @brief 设置点云ID
          */
-        void setId(const QString& id) { m_id = id; }
+        void setId(const QString &id) { m_id = id; }
 
         /**
          * @brief 设置点云包围盒
          */
-        void setBox(const Box& box) { m_box = box; }
+        void setBox(const Box &box) { m_box = box; }
 
         /**
          * @brief 设置点云文件信息
          */
-        void setInfo(const QFileInfo& info) { m_info = info; }
+        void setInfo(const QFileInfo &info) { m_info = info; }
 
         /**
          * @brief 设置点云点大小
@@ -177,17 +177,17 @@ namespace ct
         /**
          * @brief 设置点云点颜色 (aixs)
          */
-        void setCloudColor(const QString& aixs);
+        void setCloudColor(const QString &aixs);
 
         /**
          * @brief 设置包围盒颜色
          */
-        void setBoxColor(int r, int g, int b) { m_box_rgb = RGB(r, g, b); }
+        void setBoxColor(int r, int g, int b) { m_box_rgb.rgb = ((int)r) << 16 | ((int)g) << 8 | ((int)b); }
 
         /**
          * @brief 设置法线颜色
          */
-        void setNormalColor(int r, int g, int b) { m_normals_rgb = RGB(r, g, b); }
+        void setNormalColor(int r, int g, int b) { m_normals_rgb.rgb = ((int)r) << 16 | ((int)g) << 8 | ((int)b); }
 
         /**
          * @brief 设置点云透明度
@@ -209,15 +209,15 @@ namespace ct
         void update(bool resolution_flag = true, bool box_flag = true, bool type_flag = true);
 
     private:
-        Box       m_box;
-        QString   m_id;
-        RGB       m_box_rgb;
-        RGB       m_normals_rgb;
-        QString   m_type;
+        Box m_box;
+        QString m_id;
+        RGB m_box_rgb;
+        RGB m_normals_rgb;
+        QString m_type;
         QFileInfo m_info;
-        int       m_point_size;
-        float     m_opacity;
-        float     m_resolution;
+        int m_point_size;
+        float m_opacity;
+        float m_resolution;
     };
 } // namespace ct
 #endif // CT_BASE_CLOUD_H
